8481fc0d1583c3dafb426bc677db2424700a949c
sfanzalone <sfanzalone@gmail.com>
2023-03-17 11:29:42 -0400

    Got the capsule to move up, down, left, & right

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] private float steerSpeed = 1f; //Changing this value INVERTS the controls
+    [SerializeField] private float driveSpeed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,11 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, -1.1f);
-        transform.Translate(0, 0.2f, 0);
+        float steerAmt = Input.GetAxis("Horizontal") * steerSpeed; //Changing steerSpeed INVERTS the controls
+        float moveAmt = Input.GetAxis("Vertical") * driveSpeed; //Changes vertical movement
+
+        transform.Rotate(0, 0, -steerAmt); //Making steerAmt POSITIVE will INVERTS the controls
+        //transform.Translate(0, driveSpeed, 0);
+        transform.Translate(0, moveAmt, 0);
     }
 }
